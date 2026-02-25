7b093b277d3eaf6b95138db7e16eb83eede08aac
yoshiki9636 <61720120+yoshiki9636@users.noreply.github.com>
2026-02-26 06:20:23 +0900

    change for test pass

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_risc-v-wg_swc1 user_project (
+  tt_um_risc_v_wg_swc1 user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
