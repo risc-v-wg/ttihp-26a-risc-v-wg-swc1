1d39f88f788885a3ebd158e94b88f119a9b936c9
yoshiki9636 <61720120+yoshiki9636@users.noreply.github.com>
2026-02-25 20:52:52 +0900

    fixed top name typo

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_tt_um_risc-v-wg_swc1 user_project (
+  tt_um_risc-v-wg_swc1 user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
